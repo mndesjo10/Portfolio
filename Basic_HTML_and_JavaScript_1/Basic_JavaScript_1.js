0cf091ddcc0a836257c1ddd73605368644f46c96
Joel Mendes <mendesjoel07@gmail.com>
2025-07-29 11:13:04 +0100

    Basic HTML and JavaScript 1 Submission Assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,1 @@
+alert("Hello, World! This alert is from an external JS file!");
---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+  var str = "Kiss me, I'm Irish!";
+  var result = str.fontcolor("green");
+  document.getElementById("Irish").innerHTML = result;
+}
